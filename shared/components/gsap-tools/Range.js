fb76a09a5e89cbf86f19d1acd575196ba47a6145
Jérémy Barbet <jeremgraph@gmail.com>
2018-05-08 09:20:33 +0000

    Bootstrap in/out markers

--- shared/components/gsap-tools/Range.js
+++ shared/components/gsap-tools/Range.js
@@ -2,8 +2,12 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import ResizeObserver from 'resize-observer-polyfill';
 import clamp from 'lodash/clamp';
+import { TweenLite } from 'gsap';
 
 import s from './Range.scss';
+
+const HANDLE_MEDIAN_WIDTH = 10;
+const MARKER_WIDTH = 10;
 
 export default class Range extends PureComponent {
 
@@ -12,6 +16,8 @@ export default class Range extends PureComponent {
     onChange: PropTypes.func,
     onChangeStart: PropTypes.func,
     onChangeComplete: PropTypes.func,
+    onChangeMarkerIn: PropTypes.func,
+    onChangeMarkerOut: PropTypes.func,
   }
 
   static defaultProps = {
@@ -23,7 +29,6 @@ export default class Range extends PureComponent {
 
     this.state = {
       limit: 0,
-      grab: 0,
     };
   }
 
@@ -40,13 +45,10 @@ export default class Range extends PureComponent {
       return;
     }
 
-    const sliderPos = this.range.offsetWidth;
-    const handlePos = this.handle.offsetWidth;
+    const { offsetWidth: rw } = this.range;
+    const { offsetWidth: hw } = this.handle;
 
-    this.setState({
-      limit: sliderPos - handlePos,
-      grab: handlePos / 2,
-    });
+    this.setState({ limit: rw - hw });
   }
 
   handleStart = (e) => {
@@ -61,8 +63,6 @@ export default class Range extends PureComponent {
   }
 
   handleDrag = (e) => {
-    e.stopPropagation();
-
     const { onChange, onChangeStart } = this.props;
     const { currentTarget } = e;
 
@@ -72,7 +72,7 @@ export default class Range extends PureComponent {
 
     const value = this.position(e);
 
-    onChange(value, e);
+    onChange(value);
 
     if (currentTarget.id === 'range') {
       document.addEventListener('mousemove', this.handleDrag);
@@ -88,11 +88,58 @@ export default class Range extends PureComponent {
     const { onChangeComplete } = this.props;
 
     document.removeEventListener('mousemove', this.handleDrag);
+    document.removeEventListener('mousemove', this.handleDragIn);
+    document.removeEventListener('mousemove', this.handleDragOut);
     document.removeEventListener('mouseup', this.handleEnd);
 
     if (onChangeComplete) {
       onChangeComplete();
     }
+  }
+
+  handleDragIn = (e) => {
+    const { onChangeMarkerIn } = this.props;
+
+    if (!onChangeMarkerIn) {
+      return;
+    }
+
+    const value = this.position(e);
+    const { offsetWidth: rw } = this.range;
+    const left = (value * (rw - MARKER_WIDTH)) / 100;
+
+    onChangeMarkerIn(value);
+
+    TweenLite.set(
+      this.rangeIn,
+      { left },
+    );
+
+    document.addEventListener('mousemove', this.handleDragIn);
+    document.addEventListener('mouseup', this.handleEnd);
+  }
+
+  handleDragOut = (e) => {
+    const { onChangeMarkerOut } = this.props;
+
+    if (!onChangeMarkerOut) {
+      return;
+    }
+
+    const value = this.position(e);
+    const { offsetWidth: rw } = this.range;
+    const val = (value * (rw - MARKER_WIDTH)) / 100;
+    const right = (rw - MARKER_WIDTH) - val;
+
+    onChangeMarkerOut(value);
+
+    TweenLite.set(
+      this.rangeOut,
+      { right },
+    );
+
+    document.addEventListener('mousemove', this.handleDragOut);
+    document.addEventListener('mouseup', this.handleEnd);
   }
 
   getPositionFromValue = (value) => {
@@ -110,68 +157,73 @@ export default class Range extends PureComponent {
   }
 
   position = (e) => {
-    const { grab } = this.state;
     const coordinate = !e.touches ? e.clientX : e.touches[0].clientX;
     const { left } = this.range.getBoundingClientRect();
-    const pos = coordinate - left - grab;
+    const pos = coordinate - left - HANDLE_MEDIAN_WIDTH;
 
     return this.getValueFromPosition(pos);
   }
 
   coordinates = (pos) => {
-    const { grab } = this.state;
     const value = this.getValueFromPosition(pos);
     const position = this.getPositionFromValue(value);
 
-    return position + grab;
+    return position + HANDLE_MEDIAN_WIDTH;
   }
 
   render() {
     const { value } = this.props;
 
+    // TODO: Move handle with gsap instead
     const position = this.getPositionFromValue(value);
     const coords = this.coordinates(position);
     const fillStyle = { width: `${coords}px` };
     const handleStyle = { left: `${coords}px` };
 
     return (
-      <div // eslint-disable-line
-        ref={(c) => { this.range = c; }}
-        className={s.range}
-        onMouseDown={this.handleDrag}
-        onMouseUp={this.handleEnd}
-        onTouchStart={this.handleStart}
-        onTouchEnd={this.handleEnd}
-        id="range"
-      >
-        <div className={s.range__fill} style={fillStyle} />
-
+      <div className={s.range}>
         <button
           ref={(c) => { this.rangeIn = c; }}
           className={s(s.range__markers, s.range__markersIn)}
+          onMouseDown={this.handleDragIn}
+          onMouseUp={this.handleEndIn}
         >
-          <svg width="10.1" height="17.9" viewBox="0 0 10.1 17.9">
-            <path fille="#cad5db" d="M10.1,13.3L10.1,13.3l-4.2,4.4c-0.4,0.4-1,0.4-1.4,0L0,13.3l0.1-0.1V1c0-0.6,0.4-1,1-1h8c0.6,0,1,0.4,1,1L10.1,13.3L10.1,13.3z" />
+          <svg width="10" height="18" viewBox="0 0 10 18">
+            <path fille="#cad5db" d="M5.8,17.7c-0.4,0.4-0.9,0.4-1.3,0L0,13.3V1c0-0.6,0.4-1,1-1h8c0.6,0,1,0.4,1,1v12.3L5.8,17.7z" />
           </svg>
         </button>
 
         <button
           ref={(c) => { this.rangeOut = c; }}
           className={s(s.range__markers, s.range__markersOut)}
+          onMouseDown={this.handleDragOut}
+          onMouseUp={this.handleEndOut}
         >
-          <svg width="10.1" height="17.9" viewBox="0 0 10.1 17.9">
-            <path fille="#cad5db" d="M10.1,13.3L10.1,13.3l-4.2,4.4c-0.4,0.4-1,0.4-1.4,0L0,13.3l0.1-0.1V1c0-0.6,0.4-1,1-1h8c0.6,0,1,0.4,1,1L10.1,13.3L10.1,13.3z" />
+          <svg width="10" height="18" viewBox="0 0 10 18">
+            <path fille="#cad5db" d="M5.8,17.7c-0.4,0.4-0.9,0.4-1.3,0L0,13.3V1c0-0.6,0.4-1,1-1h8c0.6,0,1,0.4,1,1v12.3L5.8,17.7z" />
           </svg>
         </button>
 
-        <button
-          ref={(c) => { this.handle = c; }}
-          className={s.range__handle}
-          onMouseDown={this.handleStart}
-          onTouchMove={this.handleDrag}
+        <div // eslint-disable-line
+          ref={(c) => { this.range = c; }}
+          className={s.range__input}
+          onMouseDown={this.handleDrag}
+          onMouseUp={this.handleEnd}
+          onTouchStart={this.handleStart}
           onTouchEnd={this.handleEnd}
-          style={handleStyle}
-        />
+          id="range"
+        >
+          <div className={s.range__fill} style={fillStyle} />
+
+          <button
+            ref={(c) => { this.handle = c; }}
+            className={s.range__handle}
+            onMouseDown={this.handleStart}
+            onTouchMove={this.handleDrag}
+            onTouchEnd={this.handleEnd}
+            style={handleStyle}
+          />
+        </div>
       </div>
     );
   }
